5502ec36cbd6fd596db04a276917e1023c4b42ef
CalebHolt-15 <calebholt100@gmail.com>
2020-12-04 12:44:38 +0530

    Added timestamps to Course Entity

--- src/res/course/course.model.js
+++ src/res/course/course.model.js
@@ -1,24 +1,27 @@
 import mongoose from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate-v2';
 
-const courseSchema = new mongoose.Schema({
-  name: String,
-  section: String,
-  subjects: [
-    {
-      type: mongoose.SchemaTypes.ObjectId,
-      //   required: true,
-      ref: 'subject',
-    },
-  ],
-  school: [
-    {
-      type: mongoose.SchemaTypes.ObjectId,
-      //   required: true,
-      ref: 'school',
-    },
-  ],
-});
+const courseSchema = new mongoose.Schema(
+  {
+    name: String,
+    section: String,
+    subjects: [
+      {
+        type: mongoose.SchemaTypes.ObjectId,
+        //   required: true,
+        ref: 'subject',
+      },
+    ],
+    school: [
+      {
+        type: mongoose.SchemaTypes.ObjectId,
+        //   required: true,
+        ref: 'school',
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 courseSchema.plugin(mongoosePaginate);
 
