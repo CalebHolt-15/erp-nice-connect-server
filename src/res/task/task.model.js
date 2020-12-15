ced1a96a0f77d939e6e1be0ea672f3953150d39b
CalebHolt-15 <calebholt100@gmail.com>
2020-12-15 15:08:01 +0530

    Added ore Fileds on Task Entity

--- src/res/task/task.model.js
+++ src/res/task/task.model.js
@@ -3,41 +3,46 @@ import mongoosePaginate from 'mongoose-paginate-v2';
 
 const taskSchema = new mongoose.Schema(
   {
-    type: String,
     title: String,
+    type: String,
     marks: String,
     start: String,
     due: String,
-    questions: [
-      {
-        head: String,
-        body: String,
-        images: [String],
-        options: [String],
-        correctOption: String,
-      },
-    ],
-    comments: [
-      {
-        user_id: String,
-        name: String,
-        text: String,
-      },
-    ],
-    subject: {
-      type: mongoose.SchemaTypes.ObjectId,
-      // required: true,
-    },
-    submissions: [
-      {
-        type: mongoose.SchemaTypes.ObjectId,
-        //   required: true,
-      },
-    ],
-    user: {
-      type: mongoose.SchemaTypes.ObjectId,
-      // required: true,
-    },
+    questions: String,
+    //   [
+    //   {
+    //     head: String,
+    //     body: String,
+    //     images: [String],
+    //     options: [String],
+    //     correctOption: String,
+    //   },
+    // ],
+    comments: String,
+    //   [
+    //   {
+    //     user_id: String,
+    //     name: String,
+    //     text: String,
+    //   },
+    // ],
+    subject: String,
+    // {
+    //   type: mongoose.SchemaTypes.ObjectId,
+    //   // required: true,
+    // },
+    submissions: String,
+    // [
+    //   {
+    //     type: mongoose.SchemaTypes.ObjectId,
+    //     //   required: true,
+    //   },
+    // ],
+    user: String,
+    // {
+    //   type: mongoose.SchemaTypes.ObjectId,
+    //   // required: true,
+    // },
   },
   { timestamps: true }
 );
