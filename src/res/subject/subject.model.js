4180cd5abe9f297e10a281406a89c13c7973a7a6
rilangkishabong <rilangkishabong@gmail.com>
2020-12-29 13:28:11 +0530

    removed fields.

--- src/res/subject/subject.model.js
+++ src/res/subject/subject.model.js
@@ -4,17 +4,6 @@ import mongoosePaginate from 'mongoose-paginate-v2';
 const subjectSchema = new mongoose.Schema(
   {
     name: String,
-    totalMarks: String,
-    notifications: [{
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'notification',
-    }],
-    course: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'course'
-    }
   },
   { timestamps: true }
 );
