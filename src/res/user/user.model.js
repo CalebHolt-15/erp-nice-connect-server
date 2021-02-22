60aff0bddc1a6726b17c7c842d8c7e4d746b460c
rilangkishabong <rilangkishabong@gmail.com>
2021-02-22 12:18:42 +0530

    removed some fields for user sign up.

--- src/res/user/user.model.js
+++ src/res/user/user.model.js
@@ -3,16 +3,9 @@ import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
   {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    password: String,
+    phNo: { type: String, required: true, unique: true},
     role: { type: String, required: true },
-    firstName: String,
-    lastName: String,
+    password: { type: String, required: true },
   },
   { timestamps: true }
 );
