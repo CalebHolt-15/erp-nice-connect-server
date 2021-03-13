405015626a2623d522ecbffe89217d87dc465b21
rilangkishabong <rilangkishabong@gmail.com>
2021-03-13 18:47:45 +0530

    added the new answer options to the schema.

--- src/res/quiz/quiz.model.js
+++ src/res/quiz/quiz.model.js
@@ -2,7 +2,12 @@ import mongoose from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate-v2';
 
 const quizSchema = new mongoose.Schema({
-  questions: [{ questionText: String }],
+  questions: [
+    {
+      questionText: String,
+      answerOptions: [{ answerText: String, isCorrect: Boolean }],
+    },
+  ],
 });
 
 quizSchema.plugin(mongoosePaginate);
